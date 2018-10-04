863aa0e9e37985732656f1bafcb69cb633568702
Luis Fernando <nascimento.lsfo@gmail.com>
2018-10-04 00:00:42 -0300

    Add landing page system specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,14 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
 =begin
